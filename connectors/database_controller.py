a330ded374ff486c90e8cbeab54fe8a83a8c6439
The Wizard <thewizard@gwendolyn.life>
2022-07-16 11:22:00 -0700

    database_controller.py -> adds `show_bands()` method
    show_bands() sends a list of just the bands names which have been requested

--- connectors/database_controller.py
+++ connectors/database_controller.py
@@ -18,3 +18,6 @@ class DatabaseController(object):
 
     async def request_band(self, band_name: str) -> UpdateResult:
         return self.admin.bands.update_one({'name': band_name}, {'$inc': {'count': 1}}, upsert=True)
+
+    async def show_bands(self):
+        return [band['name'] for band in self.db.bands.find({})]
